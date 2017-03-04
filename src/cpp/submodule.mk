346154ecfd067109d3673693712e1165ac275cf3
jynn <uaggz@student.kit.edu>
2017-03-04 15:33:46 +0100

    new including makefile

---
+++ src/cpp/submodule.mk
@@ -0,0 +1,37 @@
+DIR__-HERE := DIR__
+DIR__-SRC_DIR := $(DIR__-HERE)/src
+DIR__-OBJ_DIR := $(DIR__-HERE)/obj
+DIR__-INCLUDE_DIRS := $(DIR__-HERE)/
+DIR__-DIRS := $(DIR__-INCLUDE_DIRS) $(DIR__-SRC_DIR) $(DIR__-OBJ_DIR)
+
+DIR__-CPP_FILES := $(wildcard $(DIR__-SRC_DIR)/*.cpp)
+DIR__-OBJ_FILES := $(CPP_FILES:$(DIR__-SRC_DIR)/%.cpp=$(DIR__-OBJ_DIR)/%.o)
+DIR__-DEP_FILES := $(OBJ_FILES:.o=.d)
+DIR__-MODULE_FILES := $(filter-out $(DIR__-DIRS),$(patsubst %/.,%,$(wildcard $(DIR__-HERE)/*/.)))
+DIR__-MODULE_MAKEFILES := $(DIR__-MODULE_FILES:%=%/makefile)
+DIR__-LIB_FILES := $(foreach f,$(DIR__-MODULE_FILES),$f/$f.a)
+
+DIR__-NAME := DIR__/$(notdir DIR__)
+
+$(DIR__-NAME): $(DIR__-OBJ_FILES) $(DIR__-LIB_FILES)
+	$(CXX) $(LD_FLAGS) -o $@ $(DIR__-OBJ_FILES) $(DIR__-LIB_FILES)
+
+$(DIR__-NAME).a: $(DIR__-OBJ_FILES) $(DIR__-LIB_FILES)
+	$(AR) $(AR_FLAGS) $@ $(DIR__-OBJ_FILES) $(DIR__-LIB_FILES)
+
+$(DIR__-OBJ_DIR)/%.o: $(DIR__-SRC_DIR)/%.cpp
+	@mkdir -p $(DIR__-OBJ_DIR)
+	$(CXX) $(CXX_FLAGS) -c -o $@ $<
+
+DIR__.clean: $(DIR__-MODULE_FILES:%=%.clean)
+	@$(foreach f,$(DIR__-NAME) $(DIR__-NAME).a $(DIR__-OBJ_DIR) $(DIR__-MODULE_MAKEFILES),[[ -e $f ]] && rm -r $f && echo rm -r $f || true;)
+
+$(DIR__-MODULE_MAKEFILES): $(SUBMODULE_MAKEFILE)
+	$(M4) -D DIR__=$(@:%/makefile=%) $(SUBMODULE_MAKEFILE) > $@
+
+-include $(DIR__-DEP_FILES)
+
+include $(DIR__-MODULE_MAKEFILES)
+
+.PHONY: DIR__.clean
+
